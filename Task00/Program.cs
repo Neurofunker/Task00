acfcfce3d56710ca070c45d0d8846a717cf50038
Makc <m_stefanenkov@mail.ru>
2023-01-17 12:50:11 +0300

    Решили задачу 1

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// 0. Демонстрация решения
+
+// 1.Напишите программу, которая на вход принимает число
+// 2 выдаёт его квадрат (число умноженное на само себя).
+// 3 Вывод результата
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); //3242332
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+
+
